135363fb5def097a18dde0b120ebfa4edea434cf
AaronMan <aaron.ibt@gmail.com>
2019-12-01 16:04:28 +0100

    ADD: layout.html template and index/home view CODE: agreeement with provided assets

--- team_builder/team_builder/settings.py
+++ team_builder/team_builder/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'team_builder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets/'),
+]
---
+++ team_builder/teams/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    #url(r'sign_up/', views.sing_up, name=sign_up),
+]
